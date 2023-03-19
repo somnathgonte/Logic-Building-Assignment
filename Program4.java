6a559b5f3b13cc8958dc5a982a72a3b771023910
Somnath Gonte <105125522+somnathgonte@users.noreply.github.com>
2023-03-20 00:06:58 +0530

    Add files via upload

---
+++ Program4.java
@@ -0,0 +1,25 @@
+//Accept one number from user and print even factors of that number.
+//Input : 24
+//Output : 2 4 6 8 12 
+import java.util.*;
+public class Program4 
+{
+    public static void  displayFactor(int num)
+    {
+        for(int i=1;i<=num/2;i++)
+        {
+            if(num%i==0 && i%2==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int iNo1 = sc.nextInt();
+        displayFactor(iNo1);
+    }
+    
+}
