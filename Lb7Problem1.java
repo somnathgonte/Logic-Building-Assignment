45656b8d5b356a3f05bfbd24ac36873ca904c488
Somnath Gonte <105125522+somnathgonte@users.noreply.github.com>
2023-03-24 23:51:29 +0530

    Add files via upload

---
+++ Lb7Problem1.java
@@ -0,0 +1,26 @@
+//Write a program which accept number from user and return the count of even digits.
+//Input: 2395
+//Output: 1
+
+import java.util.*;
+public class Demo
+{
+    public static void main(String []arg)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int iNo = sc.nextInt();
+
+        int count = 0;
+        while(iNo !=  0)
+        {
+            int rem = iNo%10;
+            if(rem%2 == 0)
+            {
+                count++;
+            }
+            iNo /= 10;
+        }
+        System.out.println(count);
+    }
+}
