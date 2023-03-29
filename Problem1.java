951a9fb00ca5db2c697b520eea8723987b5a6044
Somnath Gonte <105125522+somnathgonte@users.noreply.github.com>
2023-03-29 23:08:28 +0530

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,8 +1,8 @@
 // 1. Accept number of rows and number of columns from user and display below pattern. 
 // Input :  	iRow = 4  	iCol = 3 	 
 // Output : 	* 	* 	* 	
-//  	 	    * 	* 	* 	
-//  	 	    * 	* 	* 	
+//  	 	   * 	* 	* 	
+//  	 	   * 	* 	* 	
 //  	    	* 	* 	* 	
 
 import java.util.*;
