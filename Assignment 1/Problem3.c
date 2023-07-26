1b1a61b108b0617eb1ce36c61c446218c2cdf66f
Somnath Gonte <105125522+somnathgonte@users.noreply.github.com>
2023-07-26 19:15:36 +0530

    Add files via upload

---
+++ Assignment 1/Problem3.c
@@ -0,0 +1,16 @@
+// Program to print 5 to 1 number on screen
+
+#include <stdio.h>
+void Display()
+{
+    int i = 0;
+    for (i = 5; i >= 1; i--)
+    {
+        printf("%d\n", i);
+    }
+}
+int main()
+{
+    Display();
+    return 0;
+}
